c81a839554cfe1164379226a2d1f59c2b5d44e15
pereisaiah21 <isaiahpereira@proton.me>
2024-01-23 02:00:37 -0300

    Adjust bundle.js location

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ module.exports = {
     entry: './src/App.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'public/dist'),
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public/dist'),
+        publicPath: '/dist/',
+      },
     mode: 'development',
 }
